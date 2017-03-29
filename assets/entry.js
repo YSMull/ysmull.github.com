da2ecfb8aafb5b441d75954a69b9d0ee1af5dd0a
YSMull <ysmull@aliyun.com>
2017-03-29 21:36:50 +0800

    shake

--- assets/entry.js
+++ assets/entry.js
@@ -1,4 +1,5 @@
 $ = jQuery = require('./node_modules/jquery/dist/jquery.min.js')
 require('./node_modules/bootstrap/dist/css/bootstrap.min.css')
 require('./node_modules/bootstrap/dist/js/bootstrap.min.js')
+require('./css/csshake.min.css')
 require('./css/main.scss')
