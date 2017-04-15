190356d1875cbe4b7f0936b4a52446b3806c444e
YSMull <ysmull@aliyun.com>
2017-04-15 17:18:30 +0800

    shell

--- upload.sh
+++ upload.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo -e "\e[1;31mpush local change to origin master..."
-echo -e "\e[1;31m-------------------------------------"
+printf "\e[1;33m ------------------------------------- \e[0m\n"
+printf "\e[1;33m push local change to origin master... \e[0m\n"
 git push origin master
 
-echo -e "\e[1;31mpush local change to 腾讯云..."
-echo -e "\e[1;31m-------------------------------------"
+printf "\e[1;33m ----------------------------- \e[0m\n"
+printf "\e[1;33m push local change to 腾讯云... \e[0m\n"
 ssh -l ubuntu 123.207.108.34 'sh ~/upload.sh'
 
-echo -e "\e[1;31mpush local change to 阿里云..."
-echo -e "\e[1;31m-------------------------------------"
+printf "\e[1;33m ----------------------------- \e[0m\n"
+printf "\e[1;33m push local change to 阿里云... \e[0m\n"
 ssh -l maoyusu 120.24.252.243 'sh ~/upload.sh'
