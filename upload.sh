49443074a289f8e15d8b5374055a599524620783
YSMull <ysmull@aliyun.com>
2017-04-15 17:13:55 +0800

    uplaod

--- upload.sh
+++ upload.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+echo -e "\e[1;31mpush local change to origin master..."
+echo -e "\e[1;31m-------------------------------------"
 git push origin master
+
+echo -e "\e[1;31mpush local change to 腾讯云..."
+echo -e "\e[1;31m-------------------------------------"
 ssh -l ubuntu 123.207.108.34 'sh ~/upload.sh'
+
+echo -e "\e[1;31mpush local change to 阿里云..."
+echo -e "\e[1;31m-------------------------------------"
 ssh -l maoyusu 120.24.252.243 'sh ~/upload.sh'
