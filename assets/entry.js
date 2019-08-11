0b064079bd5af99190e4b376490428aaa2035f0e
毛煜苏 <maoyusu@corp.netease.com>
2019-08-11 15:34:34 +0800

    gongshi

--- assets/entry.js
+++ assets/entry.js
@@ -5,10 +5,27 @@ LazyLoad = require("vanilla-lazyload/dist/lazyload.amd");
 require('./css/pure.min.css');
 require('./css/grids-responsive-min.css');
 require('./css/main.scss');
+
 // require('clipboard');
 require('./plugins/prism/prism.js');
 require('./plugins/prism/prism.css');
 
+require('katex/dist/katex.min.css');
+
 require('gitalk/dist/gitalk.css');
 Gitalk = require('gitalk');
+
+gitalk = (id, title, labels, body) => new Gitalk({
+  clientID: 'd666a4339c38b486abf4',
+  clientSecret: 'ce81d1481ce8f64f8923f17d93f94cbf23068441',
+  repo: 'blog',
+  owner: 'YSMull',
+  admin: ['YSMull'],
+  perPage: 20,
+  id,
+  title,
+  distractionFreeMode: false,
+  labels,
+  body
+});
 // clickEval = require('./js/scheme.js')
